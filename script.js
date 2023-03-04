1d02dff338478327448e68feed73fac9dfbc14d9
Alimardon Toshpo'latov <alimardoncoder001@gmail.com>
2023-03-04 23:56:44 +0500

    added for12

---
+++ script.js
@@ -0,0 +1,75 @@
+// For1
+// let n = 3, k = 10;
+// for (let i = 0; i < n; i++) {
+//     console.log("k");
+// }
+
+// For2
+// let a = 8, b = 14;
+// console.log(b - a + 1);
+
+// For3
+// let a = 7, b = 13;
+// for (let i = b - 1; i > a; i--) {
+//     console.log(i);
+// }
+
+// For4
+// let sum = 20000;
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i * sum);
+// }
+
+// For5
+// let sum = 20000;
+// for (let i = 1; i <= 10; i++) {
+//     console.log((i * sum) / 10);
+// }
+
+// For6
+// let sum = 20000;
+// for (let i = 12; i <= 20; i++) {
+//     console.log((i * sum) / 10);
+// }
+
+// For7
+// let a = 3, b = 6, s = 0;
+// for (let i = a; i <= b; i++) {
+//     s += i;
+// }
+// console.log(s);
+
+// For8
+// let a = 3, b = 5, s = 1;
+// for (let i = a; i <= b; i++) {
+//     s *= i;
+// }
+// console.log(s);
+
+// For9
+// let a = 1, b = 3, s = 0;
+// for (let i = a; i <= b; i++) {
+//     s += i**2;
+// }
+// console.log(s);
+
+// For10
+// let n = 2, s = 0;
+// for (let i = 1; i <= n; i++) {
+//     s += 1 / i;
+// }
+// console.log(s);
+
+// For11
+// let n = 1, s = 0;
+// for (let i = 0; i <= n; i++) {
+//     s += (n + i) ** 2;
+// }
+// console.log(s);
+
+// For12
+let n = 1.3, s = 1;
+for (let i = 1; i <= n; i++) {
+    s *= (i + 0.1);
+}
+console.log(s);
